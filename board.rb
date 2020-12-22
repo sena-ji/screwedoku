72373469f027525cf7dde19159f5bf1278dfcbeb
Sena Ji <senasji@gmail.com>
2020-12-21 16:13:24 -0800

    Fixed 'filename' in Board::from_file

--- board.rb
+++ board.rb
@@ -11,7 +11,7 @@ class Board
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(&:chomp)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
       nums = row.split("").map { |char| parseInt(char) }
       nums.map { |num| Tile.new(num) }
